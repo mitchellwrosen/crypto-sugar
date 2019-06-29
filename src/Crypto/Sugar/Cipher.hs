c33f5e5317cbe2f1b38ab7cfedeb1de4491832aa
Mitchell Rosen <mitchellwrosen@gmail.com>
2019-06-29 00:12:33 -0400

    blowfish

--- src/Crypto/Sugar/Cipher.hs
+++ src/Crypto/Sugar/Cipher.hs
@@ -1,16 +1,23 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
 
 module Crypto.Sugar.Cipher
   ( aes128
   , aes192
   , aes256
+  , blowfish
+  , blowfish64
+  , blowfish128
+  , blowfish256
   ) where
 
-import Crypto.Sugar.Internal (makelen)
+import Crypto.Sugar.Internal (makeLengthExactly)
 
 import qualified Crypto.Cipher.AES as Cryptonite
+import qualified Crypto.Cipher.Blowfish as Cryptonite
 import qualified Crypto.Cipher.Types as Cryptonite
 import qualified Crypto.Error as Cryptonite
+import qualified ByteString
 
 
 aes128
@@ -18,7 +25,7 @@ aes128
   -> ByteString
   -> ByteString
 aes128 key =
-  case Cryptonite.cipherInit @Cryptonite.AES128 (makelen 16 key) of
+  case Cryptonite.cipherInit @Cryptonite.AES128 (makeLengthExactly 16 key) of
     Cryptonite.CryptoPassed cipher ->
       Cryptonite.ctrCombine cipher Cryptonite.nullIV
 
@@ -27,15 +34,50 @@ aes192
   -> ByteString
   -> ByteString
 aes192 key =
-  case Cryptonite.cipherInit @Cryptonite.AES192 (makelen 24 key) of
-    Cryptonite.CryptoPassed cipher ->
-      Cryptonite.ctrCombine cipher Cryptonite.nullIV
+  ctr @Cryptonite.AES192 (makeLengthExactly 24 key)
 
 aes256
   :: ByteString
   -> ByteString
   -> ByteString
 aes256 key =
-  case Cryptonite.cipherInit @Cryptonite.AES256 (makelen 32 key) of
+  ctr @Cryptonite.AES256 (makeLengthExactly 32 key)
+
+blowfish
+  :: ByteString
+  -> ByteString
+  -> ByteString
+blowfish key =
+  ctr @Cryptonite.Blowfish (ByteString.take 56 key)
+
+blowfish64
+  :: ByteString
+  -> ByteString
+  -> ByteString
+blowfish64 key =
+  ctr @Cryptonite.Blowfish64 (ByteString.take 56 key)
+
+blowfish128
+  :: ByteString
+  -> ByteString
+  -> ByteString
+blowfish128 key =
+  ctr @Cryptonite.Blowfish128 (ByteString.take 56 key)
+
+blowfish256
+  :: ByteString
+  -> ByteString
+  -> ByteString
+blowfish256 key =
+  ctr @Cryptonite.Blowfish256 (ByteString.take 56 key)
+
+ctr
+  :: forall cipher.
+     Cryptonite.BlockCipher cipher
+  => ByteString
+  -> ByteString
+  -> ByteString
+ctr key =
+  case Cryptonite.cipherInit @cipher key of
     Cryptonite.CryptoPassed cipher ->
       Cryptonite.ctrCombine cipher Cryptonite.nullIV
--- src/Crypto/Sugar/Internal.hs
+++ src/Crypto/Sugar/Internal.hs
@@ -1,12 +1,22 @@
 module Crypto.Sugar.Internal
-  ( makelen
+  ( makeLengthExactly
+  , makeLengthWithin
   ) where
 
 import qualified ByteString
 
 
-makelen :: Int -> ByteString -> ByteString
-makelen n (ByteString.take n -> bytes) =
+makeLengthExactly :: Int -> ByteString -> ByteString
+makeLengthExactly n =
+  makeLengthWithin n n
+
+makeLengthWithin :: Int -> Int -> ByteString -> ByteString
+makeLengthWithin n m =
+  pad n . ByteString.take m
+
+-- | Pad to at least the given length.
+pad :: Int -> ByteString -> ByteString
+pad n bytes =
   if padlen > 0
     then bytes <> ByteString.replicate padlen 0
     else bytes
